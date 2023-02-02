7ec9b52666c53567ec02df063340ab6c5fd273d5
atikahas <45572226+atikahas@users.noreply.github.com>
2023-02-02 12:42:38 +0800

    update

---
+++ assets/js/header3.js
@@ -0,0 +1,17 @@
+$(function(){
+
+    $('.menu-plate').on('click', function(e){
+      e.stopPropagation();
+    });
+  
+    $('.menu-close, header .menu-svg, .menu-overlay').on('click', function(){
+      $('html').toggleClass('menu-open');
+    });
+  
+  
+  });
+  
+  
+  
+  
+  
